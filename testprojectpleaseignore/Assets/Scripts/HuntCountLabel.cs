139dcadea2beefd9e9404980b10574f401d15331
Jesua <jesuajaen@gmail.com>
2015-01-31 23:48:28 -0500

    Added Building Cost and Count labels

---
+++ testprojectpleaseignore/Assets/Scripts/HuntCountLabel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HuntCountLabel : MonoBehaviour {
+	// Public Text Field for use with editor.
+	public Text huntCount;
+	
+	
+	// Use this for initialization
+	void Start () {
+		if(huntCount != null) {
+			huntCount.text = "0";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(huntCount != null) {
+			huntCount.text = String.Format("{0,5:N1}", BuildingTracker.instance.getCount(1));
+		}
+	}
+}
---
+++ testprojectpleaseignore/Assets/Scripts/TentCostLabel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TentCostLabel : MonoBehaviour {
+	// Public Text Field for use with editor.
+	public Text tentCost;
+	
+	
+	// Use this for initialization
+	void Start () {
+		if(tentCost != null) {
+			tentCost.text = "0";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(tentCost != null) {
+			tentCost.text = String.Format("{0,5:N1}", BuildingTracker.instance.getCost(0));
+		}
+	}
+}
