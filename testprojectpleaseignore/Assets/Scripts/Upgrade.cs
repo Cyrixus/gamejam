15554fd33b46a99591cbbeaa56e353ae61954abe
dom426 <dom624@gmail.com>
2015-01-31 22:39:11 -0500

    Adding new classes for Upgrades. Also adding that sexy Tipi picture.

---
+++ testprojectpleaseignore/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Upgrade {
+	
+	private string _name;
+	public string Name {
+		get {
+			return _name;
+		}
+	}
+	
+	private static float _cost;
+	public float Cost {
+		get {
+			return _cost;
+		}
+	}
+	
+	private static UpgradeEffect _effect;
+	public UpgradeEffect Effect {
+		get {
+			return _effect;
+		}
+	}
+
+	private bool IsActive;
+	public bool _isActive {
+		get {
+			return _isActive;
+		}
+		set {
+			_isActive = value;
+		}
+	}
+	public Upgrade (string name, float cost, UpgradeEffect effect) 
+	{
+		_name = name;
+		_cost = cost;
+		_effect = effect;
+		_isActive = false;
+	}
+
+}
---
+++ testprojectpleaseignore/Assets/Scripts/UpgradeEffect.cs
@@ -0,0 +1,21 @@
+﻿public class UpgradeEffect {
+
+	private object _target;
+	public object Target {
+		get {
+			return _target;
+		}
+	}
+
+	private static float _multiplier;
+	public float Multiplier {
+		get {
+			return _multiplier;
+		}
+	}
+
+	public UpgradeEffect (object target, float multiplier) {
+		_multiplier = multiplier;
+		_target = target;
+	}
+}
