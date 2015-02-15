5b5ed6c917964c8367e798108769e85e2eb9a22b
Matt <cryptex.md@gmail.com>
2015-02-14 22:16:34 -0500

    MISSILE DEATH

---
+++ missilemassacre/Assets/Scripts/MissileDeath.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MissileDeath : MonoBehaviour {
+	/** Public GUI Variables **/
+	public int initialMissiles = 10;
+	public int idealMissileCount = 50;
+	public float missileFrequency = 0.33f;
+
+	public Transform missilePrefab;
+
+	/** Private State **/
+	private List<Transform> missiles = new List<Transform> ();
+
+	private float elapsed = 0.0f;
+
+
+	/** Lifecycle Methods **/
+	// Use this for initialization
+	void Start () {
+		elapsed = Random.value;
+
+		while (missiles.Count < initialMissiles) {
+			buildMissile();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+
+		if (elapsed > missileFrequency && missiles.Count < idealMissileCount) {
+			buildMissile();
+			elapsed %= missileFrequency;
+		}
+	}
+
+	/** Missile builder **/
+	Transform buildMissile() {
+		Vector3 position = transform.position;
+		position.x += (Random.value * transform.localScale.x) - (transform.localScale.x / 2.0f);
+		position.y += (Random.value * transform.localScale.y) - (transform.localScale.y / 2.0f);
+		
+		// Create a new missile prefab and add it to the array of missiles
+		Transform missile = Instantiate(missilePrefab, position, Quaternion.identity) as Transform;
+
+		// Scale Randomly between 1.0f and 2.0f
+		float scalar = Random.value;
+		missile.transform.localScale = new Vector3(1.0f + scalar, 1.0f + scalar, 1.0f);
+
+		missiles.Add(missile);
+		return missile;
+	}
+}
---
+++ missilemassacre/Assets/Scripts/MissileWobble.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileWobble : MonoBehaviour {
+	public float wobbleDegree = 6.0f; // How far from side to side the missile should wobble.
+	public float frequency = 0.4f;
+
+	private float elapsed;
+	private float lastRotation;
+
+	// Use this for initialization
+	void Start () {
+		elapsed = 0.0f;
+		lastRotation = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+
+		float currentRotation = 0.0f;
+		if (wobbleDegree != 0.0f) {
+			currentRotation = Mathf.Sin (elapsed / frequency) * wobbleDegree;
+		}
+
+		transform.Rotate (0, 0, currentRotation - lastRotation);
+		lastRotation = currentRotation;
+	}
+}
