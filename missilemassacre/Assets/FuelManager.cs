54f8d90a5b6e71f8f5337670cf912faaad5ff3fb
dom426 <dom624@gmail.com>
2015-02-14 22:21:12 -0500

    still working on fuel stuff...

---
+++ missilemassacre/Assets/FuelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelManager : MonoBehaviour {
+	public GameObject energyPrefab;
+	public Transform node;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (energyPrefab, node.transform.position, node.transform.rotation);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ missilemassacre/Assets/Scripts/FuelCanisterBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelCanisterBehavior : MonoBehaviour 
+{
+	public float FallRate;
+	public Vector3 StartPosition;
+	public float EnergyLevel;
+	public Sprite Sprite;
+
+	public FuelCanisterBehavior (float speed, Vector3 start, float level, Sprite sprite)
+	{
+		this.FallRate = speed;
+		this.StartPosition = start;
+		this.EnergyLevel = level;
+		this.Sprite = sprite;
+	}
+
+	public void DropItLikeItsHot() 
+	{
+		gameObject.transform.position = this.StartPosition;
+		gameObject.rigidbody2D.velocity = new Vector2(0f, -this.FallRate);
+	}
+	
+	private void OnTriggerEnter2D (Collider2D hitBox)
+	{
+		if (hitBox.name.Equals("PlayerShip"))
+		{
+			Debug.Log("Ship energy +" + this.EnergyLevel.ToString () + "!");
+		}
+	}
+}
