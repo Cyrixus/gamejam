589d735a387e29369c792e96272db484b2e1bc27
Matt <cryptex.md@gmail.com>
2015-02-14 22:50:42 -0500

    Integrated FuelBar with MISSLE DEATH, and repositioned on the screen.

---
+++ missilemassacre/Assets/Scripts/FuelTracker.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelTracker : MonoBehaviour {
+
+	private float fuelDecRate;
+	private float currentFuel;
+
+	public static FuelTracker instance {get; private set;}
+
+	// Use this for initialization
+	void Start () {
+		currentFuel = 1.0f;		//100% Fuel
+		fuelDecRate = .001f;
+	}
+
+	void Awake() {
+		// Update our singleton to the current active instance
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentFuel -= fuelDecRate;
+		if (currentFuel < 0) 
+			currentFuel = 0;
+	}
+
+	public float getFuel () {
+		return currentFuel;
+	}
+}
