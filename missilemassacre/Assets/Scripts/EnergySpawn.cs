f719213b839d5ac692a2a5535463d18027be001c
Matt <cryptex.md@gmail.com>
2015-02-15 02:31:44 -0500

    BONUS COMMIT NUMERO UNO.

--- missilemassacre/Assets/Scripts/EnergySpawn.cs
+++ missilemassacre/Assets/Scripts/EnergySpawn.cs
@@ -6,7 +6,6 @@ public class EnergySpawn : MonoBehaviour {
 	/** Public GUI Variables **/
 
 	public Transform lowEnergyPrefab;
-	public float lowEnergyChance = 1f;
 
 	public Transform midEnergyPrefab;
 	public float midEnergyChance = 0.3f;
@@ -15,14 +14,16 @@ public class EnergySpawn : MonoBehaviour {
 	public float highEnergyChance = 0.01f;
 
 	public float frequency = 1.0f;
-	
-	private float lowerBound = 0f;
-	private float upperBound = 1f;
 
+	/** Private State **/
 	private float elapsed = 0.0f;
+	private Vector3 lastSpawnPosition = Vector3.zero;
 
+
+	/** Lifecycle Methods **/
 	void Start() {
 		elapsed = 0.0f;
+		lastSpawnPosition = transform.position;
 	}
 
 	// Update is called once per frame
@@ -32,27 +33,61 @@ public class EnergySpawn : MonoBehaviour {
 		// only spawn an energy if there isn't one available already
 		if (elapsed > frequency) {		
 			// pull a random value and spawn level based on it
-			float randomValue = Random.Range (lowerBound, upperBound);
+			float randomValue = Random.value;
 			
-			if (randomValue <= this.lowEnergyChance) {
-				buildEnergy (this.lowEnergyPrefab);
-			} else if (randomValue <= this.midEnergyChance) {
-				buildEnergy (this.midEnergyPrefab);
+			if (randomValue < highEnergyChance) {
+				spawnHighEnergy();
+			} else if (randomValue < midEnergyChance + highEnergyChance) {
+				spawnMediumEnergy();
 			} else {
-				buildEnergy (this.highEnergyPrefab);
+				spawnLowEnergy();
 			}
 
 			elapsed %= frequency;
 		}
 	}
 
-	/** Energy builder **/
-	void buildEnergy(Transform prefab) {
-		Vector3 position = transform.position;
-		position.x += (Random.value * transform.localScale.x) - (transform.localScale.x / 2.0f);
-		position.y += (Random.value * transform.localScale.y) - (transform.localScale.y / 2.0f);
+	/** Spawn Energy Methods **/
+	private void spawnHighEnergy() {
+		Vector3 position = lastSpawnPosition;
+		position.x += (Random.value - 0.5f) * 2.0f * (transform.localScale.x / 2.0f);
+		position.y = Random.value * transform.localScale.y + transform.localScale.y / 2.0f;
+
+		position = clampToRegion (position);
+
+		Instantiate (highEnergyPrefab, position, Quaternion.identity);
+		lastSpawnPosition = position;
+	}
+
+	private void spawnMediumEnergy() {
+		Vector3 position = lastSpawnPosition;
+		position.x += (Random.value - 0.5f) * 1.0f * (transform.localScale.x / 2.0f);
+		position.y = Random.value * transform.localScale.y + transform.localScale.y / 2.0f;
 		
-		// Create a new energy prefab
-		Instantiate (prefab, position, Quaternion.identity);
+		position = clampToRegion (position);
+		
+		Instantiate (midEnergyPrefab, position, Quaternion.identity);
+		lastSpawnPosition = position;
+	}
+
+	private void spawnLowEnergy() {
+		Vector3 position = lastSpawnPosition;
+		position.x += (Random.value - 0.5f) * 0.5f * (transform.localScale.x / 2.0f);
+		position.y = Random.value * transform.localScale.y + transform.localScale.y / 2.0f;
+		
+		position = clampToRegion (position);
+		
+		Instantiate (lowEnergyPrefab, position, Quaternion.identity);
+		lastSpawnPosition = position;
+	}
+
+	/** Helper Methods **/
+	private Vector3 clampToRegion(Vector3 position) {
+		Vector3 output = Vector3.zero;
+		output.x = Mathf.Clamp (position.x, transform.position.x - transform.localScale.x / 2.0f, transform.position.x + transform.localScale.x / 2.0f);
+		output.y = Mathf.Clamp (position.y, transform.position.y - transform.localScale.y / 2.0f, transform.position.y + transform.localScale.y / 2.0f);
+		output.z = transform.position.z;
+
+		return output;
 	}
 }
--- missilemassacre/Assets/Scripts/FuelCanisterBehavior.cs
+++ missilemassacre/Assets/Scripts/FuelCanisterBehavior.cs
@@ -24,7 +24,6 @@ public class FuelCanisterBehavior : MonoBehaviour
 	
 	void OnCollisionEnter2D (Collision2D collisionInfo)
 	{
-		Debug.Log (collisionInfo.collider.tag);
 		if (collisionInfo.collider.tag == "Player")
 		{
 			FuelTracker.instance.gainFuel(EnergyLevel);
