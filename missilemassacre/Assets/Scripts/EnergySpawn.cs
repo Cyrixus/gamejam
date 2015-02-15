64a41a280d3180c06d3b47d5021fe9e198f05ea6
dom426 <dom624@gmail.com>
2015-02-15 00:22:05 -0500

    more progress on fuel

---
+++ missilemassacre/Assets/Scripts/EnergySpawn.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnergySpawn : MonoBehaviour {
+	/** Public GUI Variables **/
+
+	public GameObject lowEnergyPrefab;
+	public float lowEnergyChance = 1f;
+
+	public GameObject midEnergyPrefab;
+	public float midEnergyChance = 0.3f;
+
+	public GameObject highEnergyPrefab;
+	public float highEnergyChance = 0.01f;
+
+	public bool isEnergyOnScreen = false;
+	
+	private float lowerBound = 0f;
+	private float upperBound = 1f;
+
+	void Start() {
+		spawnEnergies ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		spawnEnergies ();
+	}
+
+	void spawnEnergies() {
+		// only spawn an energy if there isn't one available already
+		if (!this.isEnergyOnScreen) {		
+			// pull a random value and spawn level based on it
+			float randomValue = Random.Range (lowerBound, upperBound);
+
+			if (randomValue <= this.lowEnergyChance) {
+				buildEnergy (this.lowEnergyPrefab);
+			} else if (randomValue <= this.midEnergyChance) {
+				buildEnergy (this.midEnergyPrefab);
+			} else {
+				buildEnergy (this.highEnergyPrefab);
+			}
+		}
+	}
+
+	/** Energy builder **/
+	void buildEnergy(GameObject prefab) {
+		Vector3 position = transform.position;
+		Debug.Log((Random.Range (0f, transform.localScale.x)/2f).ToString());
+		position.x = 5.0f;
+		position.y -= transform.localScale.y/2f;
+		
+		// Create a new energy prefab
+		Transform energy = Instantiate(prefab, position, Quaternion.identity) as Transform;
+
+		// Drop it likes its hot!
+		energy.SendMessage ("DropItLikeItsHot"); 
+
+		isEnergyOnScreen = true;
+	}
+}
--- missilemassacre/Assets/Scripts/FuelCanisterBehavior.cs
+++ missilemassacre/Assets/Scripts/FuelCanisterBehavior.cs
@@ -4,21 +4,10 @@ using System.Collections;
 public class FuelCanisterBehavior : MonoBehaviour 
 {
 	public float FallRate;
-	public Vector3 StartPosition;
 	public float EnergyLevel;
-	public Sprite Sprite;
-
-	public FuelCanisterBehavior (float speed, Vector3 start, float level, Sprite sprite)
-	{
-		this.FallRate = speed;
-		this.StartPosition = start;
-		this.EnergyLevel = level;
-		this.Sprite = sprite;
-	}
 
 	public void DropItLikeItsHot() 
 	{
-		gameObject.transform.position = this.StartPosition;
 		gameObject.rigidbody2D.velocity = new Vector2(0f, -this.FallRate);
 	}
 	
@@ -27,6 +16,7 @@ public class FuelCanisterBehavior : MonoBehaviour
 		if (hitBox.name.Equals("PlayerShip"))
 		{
 			Debug.Log("Ship energy +" + this.EnergyLevel.ToString () + "!");
+			Destroy(this.transform);
 		}
 	}
 }
