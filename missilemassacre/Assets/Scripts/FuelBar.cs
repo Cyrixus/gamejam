b2bae9925cd1663c5bb0eaad35bc4f16f0264723
Jesua <jesuajaen@gmail.com>
2015-02-14 21:05:48 -0500

    Added Fuel Tracking and Bar

---
+++ missilemassacre/Assets/Scripts/FuelBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FuelBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		transform.width = FUEL_BAR_WIDTH;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector2(FuelTracker.instance.getFuel(),1);
+	}
+	
+}
