22e1c178f6fffa5bfc993c4d0c30156ef36456df
Dcallies <dtcallies@gmail.com>
2015-02-16 18:03:35 -0500

    [Missiles] Now seek from left to right in the missile wall

--- missilemassacre/Assets/Scripts/MissileWobble.cs
+++ missilemassacre/Assets/Scripts/MissileWobble.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MissileWobble : MonoBehaviour {
+public class MissileWobble : MonoBehaviour
+{
 	public float wobbleDegree = 6.0f; // How far from side to side the missile should wobble.
 	public float frequency = 0.4f;
+	public float wanderAmount;
 
 	private float elapsed;
 	private float lastRotation;
+	private float anchorX;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		elapsed = 0.0f;
 		lastRotation = 0.0f;
+		anchorX = transform.position.x;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		elapsed += Time.deltaTime;
-
+		
 		float currentRotation = 0.0f;
+		float currWander = 0;
 		if (wobbleDegree != 0.0f) {
-			currentRotation = Mathf.Sin (elapsed / frequency) * wobbleDegree;
+			float amount = Mathf.Sin (elapsed / frequency);
+			// Will eventually overflow and cause missiles not to wobble as much
+			currentRotation = amount * wobbleDegree;
+			currWander = amount * wanderAmount;
 		}
-
 		transform.Rotate (0, 0, currentRotation - lastRotation);
+		transform.position = new Vector3 (anchorX + currWander, transform.position.y, transform.position.z);
+		
 		lastRotation = currentRotation;
 	}
 }
